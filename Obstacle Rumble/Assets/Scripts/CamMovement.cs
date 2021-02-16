e4de1621d9bb4d2009912a461ae32ce89fe35d71
Benas <steponaviciusbenas@gmail.com>
2021-02-16 13:28:26 +0200

    Created character controller
    Character can now run in all directions with animations and look around.
    Jumping and falling is not implemented yet.

---
+++ Obstacle Rumble/Assets/Scripts/CamMovement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class CamMovement : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    [SerializeField] private float mouseSpeed = 10f;
+    [SerializeField] private float camSmooth = 0.15f;
+
+    [SerializeField] private float offset;
+
+    private Vector3 smoothingVelocity;
+    private Vector3 currentRotation;
+
+    private float mouseX, mouseY;
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    void LateUpdate()
+    {
+        mouseX += Input.GetAxis("Mouse X") * mouseSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * mouseSpeed;
+        mouseY = Mathf.Clamp(mouseY, -5, 70);
+        
+        currentRotation = Vector3.SmoothDamp(currentRotation,
+            new Vector3(mouseY,mouseX),ref smoothingVelocity, camSmooth);
+
+        transform.eulerAngles = currentRotation;
+
+        transform.position = target.position - transform.forward * offset;
+
+    }
+}
---
+++ Obstacle Rumble/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private CharacterController player;
+
+    [SerializeField] private Animator anim;
+
+    [SerializeField] private float movementSpeed, turnSmoothTime;
+
+    private float turnSmooth;
+
+
+
+    private Vector2 moveDir;
+
+    private Transform camTransform;
+
+    private void Start()
+    {
+        camTransform = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        var inputDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveDir = inputDir.normalized;
+
+        Move();
+        Animate(inputDir.y, inputDir.x);
+        Rotate();
+    }
+
+    void Move()
+    {
+        player.Move(transform.forward * movementSpeed * moveDir.magnitude * Time.deltaTime);
+    }
+
+    void Animate(float vertical, float horizontal)
+    {
+        if (vertical > 0 || horizontal > 0)
+        {
+            anim.SetFloat("Speed", 1f, 0.05f, Time.deltaTime);
+        }
+        else if (vertical < 0 || horizontal < 0)
+        {
+            anim.SetFloat("Speed", 1f, 0.05f, Time.deltaTime);
+        }
+        else anim.SetFloat("Speed", 0f, 0.05f, Time.deltaTime);
+    }
+
+    void Rotate()
+    {
+        if (moveDir!= Vector2.zero)
+        {
+            float targetRotation = Mathf.Atan2(moveDir.x, moveDir.y)
+                * Mathf.Rad2Deg + camTransform.eulerAngles.y;
+
+            transform.eulerAngles = Vector3.up *
+                                    Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, 
+                                        ref turnSmooth,turnSmoothTime);
+        }
+        
+    }
+
+
+}
+
