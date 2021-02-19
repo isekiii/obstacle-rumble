4c4fa551c1de63fe635e09b1e2ece61666b3be87
Daiva-T <78407423+Daiva-T@users.noreply.github.com>
2021-02-19 13:17:54 +0200

    Base update

---
+++ Obstacle Rumble/Assets/Scripts/ObstacleRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleRotation : MonoBehaviour
+{
+    
+    public Transform Obj;
+    public float rotationSpeed = 3f;
+    
+    void Update()
+    {
+        Obj.transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
+}
