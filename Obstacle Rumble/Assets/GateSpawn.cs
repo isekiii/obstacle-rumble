4fffbc572f943a70d0a2029f607b0b1715448510
Benas <steponaviciusbenas@gmail.com>
2021-03-01 10:18:04 +0200

    Added gates to prevent player from leaving the arena

---
+++ Obstacle Rumble/Assets/GateSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateSpawn : MonoBehaviour
+{
+   [SerializeField] private GameObject gatePref;
+   [SerializeField] private Transform spawnPoint;
+
+   private void OnTriggerExit(Collider other)
+   {
+      if (other.gameObject.tag == "Player" || other.gameObject.tag == "Enemy")
+      {
+         Instantiate(gatePref, spawnPoint.position, spawnPoint.rotation);
+      }
+   }
+}
