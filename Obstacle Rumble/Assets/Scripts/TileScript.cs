63b08f5548d72990a012c525917cd731e39cc5ba
Daiva-T <daiva.trepkute@ktu.edu>
2021-04-06 15:28:40 +0300

    update

---
+++ Obstacle Rumble/Assets/Scripts/TileScript.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class TileScript : MonoBehaviour
+{
+    public float AnimSpeed = 0.5f;
+    public float fallTimer = 2f;
+    public Animation anim;
+    Rigidbody rb;
+
+    
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        anim["TileWiggle"].speed = AnimSpeed;
+        rb.constraints = RigidbodyConstraints.FreezePosition;
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag != "Player") return;
+        anim.Play();
+        StartCoroutine(FallDown());
+
+    }
+
+    IEnumerator FallDown()
+    {
+        yield return new WaitForSeconds(fallTimer);
+        anim.Stop();
+        rb.constraints = RigidbodyConstraints.None;
+    }
+}
