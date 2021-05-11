65a38035d024b977d2758f3063137cd041822a78
Benas Steponavicius <steponaviciusbenas@gmail.com>
2021-05-11 09:54:26 +0300

    highscore system added

--- Obstacle Rumble/Assets/Scripts/EndMenu.cs
+++ Obstacle Rumble/Assets/Scripts/EndMenu.cs
@@ -9,6 +9,8 @@ public class EndMenu : MonoBehaviour
 {
     [SerializeField] private AudioSource click;
     [SerializeField] private TMP_Text totalScore;
+    [SerializeField] private TMP_InputField inputField;
+    [SerializeField] private TMP_Text bestScore;
     public void ExitGame()
     { 
         click.Play();
@@ -26,6 +28,22 @@ public class EndMenu : MonoBehaviour
     {
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
+        
+        
         totalScore.text = PlayerPrefs.GetInt("totalScore").ToString();
     }
+
+    public void EnterName()
+    {
+        if ( PlayerPrefs.GetInt("totalScore") > PlayerPrefs.GetInt("totalBest") )
+        {
+            PlayerPrefs.SetString("BestName", inputField.text);
+            PlayerPrefs.SetInt("totalBest", PlayerPrefs.GetInt("totalScore"));
+        }
+
+        bestScore.text = $"Best player : {PlayerPrefs.GetString("BestName")}  -  {PlayerPrefs.GetInt("totalBest")}";
+    }
+    
+    
+    
 }
